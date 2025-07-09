e719a002946c6989eabe3ed2ab833c49de57b4c7
Tinsae <etoboytinsu@gmail.com>
2025-07-09 16:05:33 +0300

    Initialize React app with Vite

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
@@ -14,6 +15,12 @@ function App() {
   return (
     <>
     <WelcomeMessage />
+    <UserProfile
+  name="Alice"
+  age="25"
+  bio="Loves hiking and photography"
+/>
+
 
       <div>
         <a href="https://vite.dev" target="_blank">
