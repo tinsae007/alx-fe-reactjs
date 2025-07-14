0153c2547cafcca8348e2512c8b865435320577b
Tinsae <etoboytinsu@gmail.com>
2025-07-14 13:15:30 +0300

    hh

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,32 +7,20 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import React from 'react';
-import Counter from './components/Counter';
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
 
 
 function App() {
-   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
   const [count, setCount] = useState(0)
 
   return (
-    
     <>
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
     <WelcomeMessage />
     <UserProfile
   name="Alice"
   age="25"
   bio="Loves hiking and photography"
 />
-   <div>
-      {/* Other components */}
-      <Counter />
-    </div>
+
 
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -55,7 +43,6 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-    
   )
 }
 function App() {
