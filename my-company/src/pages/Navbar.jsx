d9a8392708407c3d8fb92f2eafcfb19f7bdb81a6
Tinsae <etoboytinsu@gmail.com>
2025-07-14 14:36:45 +0300

    hh

--- my-company/src/pages/Navbar.jsx
+++ my-company/src/pages/Navbar.jsx
@@ -2,14 +2,12 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav
-      style={{
-        display: 'flex',
-        justifyContent: 'space-around',
-        backgroundColor: '#333',
-        padding: '10px'
-      }}
-    >
+    <nav style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      backgroundColor: '#333',
+      padding: '10px'
+    }}>
       <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
       <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
       <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
