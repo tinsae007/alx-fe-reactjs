175852c3664992c26fb0a8cf69c2811993341b03
Tinsae <etoboytinsu@gmail.com>
2025-07-14 14:16:40 +0300

    hh

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,15 @@
-const UserProfile = (props) => {
+// src/components/UserProfile.jsx
+import React, { useContext } from 'react';
+import UserContext from '../UserContext'; // make sure the path is correct
+
+const UserProfile = () => {
+  const userData = useContext(UserContext);
+
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'blue' }}>{userData.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold' }}>{userData.age}</span></p>
+      <p>Bio: {userData.bio}</p>
     </div>
   );
 };
