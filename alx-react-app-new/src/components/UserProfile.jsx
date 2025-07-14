4a5d150644b6c345ea99563f6fb11d557e60716e
Tinsae <etoboytinsu@gmail.com>
2025-07-14 12:55:47 +0300

    hh

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function UserProfile(props) {
   return (
-    <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '5px' }}>
-      <h2 style={{ color: 'blue', fontSize: '1.5em' }}>{props.name}</h2>
+     <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+         <h2 style={{ color: 'blue' }}>{props.name}</h2>
       <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p>Bio: <span style={{ fontStyle: 'italic' }}>{props.bio}</span></p>
-    </div>
+       <p>Bio: {props.bio}</p>
+ </div>
   );
 }
 
