87becfff3dabb48686d0129bbf421f3e949e2185
Tinsae <etoboytinsu@gmail.com>
2025-07-14 14:12:51 +0300

    hh

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,13 +7,20 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
     <WelcomeMessage />
     <UserProfile
   name="Alice"
