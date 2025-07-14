2d54104cd6d4ab765dc9ee52ca5a6088c6f7edab
Tinsae <etoboytinsu@gmail.com>
2025-07-14 12:53:10 +0300

    hh

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Footer() {
+  return (
+     <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
