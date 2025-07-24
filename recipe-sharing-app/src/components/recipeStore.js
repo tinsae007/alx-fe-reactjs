c9243b488572c4e03bc67bca2c784e7b8e60b115
Tinsae <etoboytinsu@gmail.com>
2025-07-24 12:20:30 +0300

    hi

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,7 +1,8 @@
-import create from 'zustand'
+import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes }),
-}))
+}));
