66d3e2a68c0ea70bbcf0e726c69b5f0b20c0a7fc
Tinsae <etoboytinsu@gmail.com>
2025-07-29 12:41:07 +0300

    add file

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export const fetchAdvancedUsers = async ({ username, location, repos }) => {
+export const fetchAdvancedUsers = async ({ username, location, minRepos }) => {
   let query = '';
 
   if (username) query += `${username} in:login `;
   if (location) query += `location:${location} `;
-  if (repos) query += `repos:>=${repos}`;
+  if (minRepos) query += `repos:>=${minRepos}`;
 
   const response = await axios.get(
     `https://api.github.com/search/users?q=${encodeURIComponent(query.trim())}&per_page=10`
