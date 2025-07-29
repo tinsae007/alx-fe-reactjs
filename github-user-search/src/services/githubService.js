e856b146b31ee04cd7814d53b656d02f69344b78
Tinsae <etoboytinsu@gmail.com>
2025-07-29 13:54:13 +0300

    add file

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,22 +1,17 @@
 import axios from 'axios';
 
-export const fetchAdvancedUsers = async ({ username, location, minRepos }) => {
+const token = import.meta.env.VITE_GITHUB_API_KEY;
+const headers = token ? { Authorization: `token ${token}` } : {};
+
+export const fetchUserData = async ({ username, location, minRepos }) => {
   let query = '';
 
   if (username) query += `${username} in:login `;
   if (location) query += `location:${location} `;
   if (minRepos) query += `repos:>=${minRepos}`;
 
-  const response = await axios.get(
-    `https://api.github.com/search/users?q=${encodeURIComponent(query.trim())}&per_page=10`
-  );
+  const url = `https://api.github.com/search/users?q=${encodeURIComponent(query.trim())}&per_page=10`;
 
+  const response = await axios.get(url, { headers });
   return response.data;
 };
-const token = import.meta.env.VITE_GITHUB_API_KEY;
-
-const headers = token
-  ? { Authorization: `token ${token}` }
-  : {};
-
-const response = await axios.get(url, { headers });
