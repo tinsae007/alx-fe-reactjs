91c23776558e69801d62d1aeaf01f929f8f9b6c3
Tinsae <etoboytinsu@gmail.com>
2025-07-29 11:45:03 +0300

    add file

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -19,7 +19,8 @@ const Search = () => {
       const data = await fetchUserData(username);
       setUser(data);
     } catch (err) {
-      setError('Looks like we can’t find the user');
+      setError('Looks like we cant find the user');
+
     } finally {
       setLoading(false);
     }
