61a9cc65433708534b6c6b33aa019b153b2ae5f9
Tinsae <etoboytinsu@gmail.com>
2025-08-10 13:46:18 +0300

    css tawy

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,11 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 sm:p-4 md:p-8 max-w-sm sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
-
+    <div className="bg-gray-100 p-4 sm:p-8 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
       <img
-  src="https://via.placeholder.com/150"
-  alt="User"
-  className="rounded-full mx-auto w-36 h-36 sm:w-24 sm:h-24"
-/>
-
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="rounded-full mx-auto w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36"
+      />
       <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new technologies.
