2f13838b73f9a5120a93d7696a61d7da2fe8ca7c
Tinsae <etoboytinsu@gmail.com>
2025-08-10 13:44:47 +0300

    css tawy

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,10 +3,11 @@ function UserProfile() {
     <div className="bg-gray-100 p-8 sm:p-4 md:p-8 max-w-sm sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
 
       <img
-        src="https://via.placeholder.com/150"
-        alt="User"
-        className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto"
-      />
+  src="https://via.placeholder.com/150"
+  alt="User"
+  className="rounded-full mx-auto w-36 h-36 sm:w-24 sm:h-24"
+/>
+
       <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new technologies.
