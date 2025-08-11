563016bfb9189043d976111d908eaedd1f628512
Tinsae <etoboytinsu@gmail.com>
2025-08-11 09:47:52 +0300

    Initial React + Tailwind CSS setup for Recipe Sharing Platform

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
